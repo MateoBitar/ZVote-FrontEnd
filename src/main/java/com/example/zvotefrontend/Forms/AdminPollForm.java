b6c50c9715bb63a639582a6aaad913dfea1daa88
Marco <bitar.marco21@gmail.com>
2025-05-24 23:23:01 +0300

    .

--- src/main/java/com/example/zvotefrontend/Forms/AdminPollForm.java
+++ src/main/java/com/example/zvotefrontend/Forms/AdminPollForm.java
@@ -1,8 +1,10 @@
 package com.example.zvotefrontend.Forms;
 
 import com.example.zvotefrontend.Controllers.AdminPollController;
+import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.geometry.VPos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.effect.DropShadow;
@@ -10,110 +12,171 @@ import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
-import org.json.JSONObject;
 
-import java.util.Map;
+import java.sql.Date;
+import java.time.LocalDate;
+import java.time.ZoneId;
+
 
 public class AdminPollForm {
-    private GridPane pollForm;
-    private TextField pollTitleField, pollDescriptionField;
-    private DatePicker startDatePicker, endDatePicker;
-    private AdminPollController controller;
 
-    public void showCreatePoll(Stage primaryStage, Map<String, String> usersession) {
-        this.controller = new AdminPollController();
+    private Stage primaryStage;
+    // Method to display the "Create Poll" form
+    public void showCreatePoll(Stage primaryStage, UserModel user) throws Exception {
 
+        // Main layout
         BorderPane layout = new BorderPane();
         layout.setStyle("-fx-background-color: transparent;");
 
-        setupTopBar(layout);
-        setupForm(layout);
 
-        Scene scene = new Scene(layout, 800, 600);
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
-
-    private void setupTopBar(BorderPane layout) {
+        // Top bar configuration
         HBox topBar = new HBox(20);
         topBar.setPadding(new Insets(10, 10, 10, 40));
         topBar.setStyle("-fx-background-color: #C8F0FF;");
         topBar.setAlignment(Pos.CENTER_LEFT);
 
+        // Add shadow effect
         DropShadow shadow = new DropShadow();
         shadow.setRadius(5);
         shadow.setOffsetY(2);
         shadow.setColor(Color.LIGHTGRAY);
         topBar.setEffect(shadow);
 
+        // Logo
         Label logo = new Label("ZVote");
         logo.setFont(Font.font("Onyx", FontWeight.BOLD, 60));
 
+        // Back button
         Button backButton = new Button("Back");
-        backButton.setOnAction(event -> primaryStage.close());
+        backButton.setStyle("-fx-background-color: #C8F0FF; -fx-font-weight: bold; -fx-border-radius: 10px; -fx-font-size: 22px;" +
+                "-fx-cursor: hand");
+        backButton.setOnAction(event -> {
+            try {
+                AdminLandingPageController adminLandingPageController = new AdminLandingPageController();
+                UserController.userSession.put("user", user);
+                adminLandingPageController.showAdminLandingPage(primaryStage, UserController.userSession);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
 
+        // Align elements within the top bar
         Region spacer = new Region();
         HBox.setHgrow(spacer, Priority.ALWAYS);
-
         topBar.getChildren().addAll(logo, spacer, backButton);
         layout.setTop(topBar);
-    }
 
-    private void setupForm(BorderPane layout) {
-        pollForm = new GridPane();
-        pollForm.setPadding(new Insets(20));
-        pollForm.setAlignment(Pos.TOP_CENTER);
-        pollForm.setHgap(20);
-        pollForm.setVgap(20);
 
+        // Create the poll form
+        GridPane createPollForm = new GridPane();
+        createPollForm.setStyle("-fx-background-color: #FFFFFF; -fx-border-color: #C8F0FF; -fx-border-width: 3px;");
+        createPollForm.setPadding(new Insets(20));
+        createPollForm.setAlignment(Pos.TOP_CENTER);
+        createPollForm.setHgap(20);
+        createPollForm.setVgap(20);
+
+        // Form Title
         Label title = new Label("Create Poll");
-        title.setStyle("-fx-font-size: 30px; -fx-font-weight: bold;");
-        pollForm.add(title, 0, 0, 2, 1);
+        title.setStyle("-fx-font-size: 50px; -fx-font-weight: bold;");
+        title.setPadding(new Insets(0, 0, 0, 10));
+        createPollForm.add(title, 0, 0);
+        GridPane.setColumnSpan(title, 2);
+        GridPane.setHalignment(title, HPos.CENTER);
+        GridPane.setValignment(title, VPos.CENTER);
 
-        pollTitleField = new TextField();
+
+        // Poll fields
+        Label pollTitleLabel = new Label("Poll Title:");
+        pollTitleLabel.setStyle("-fx-font-size: 20px; -fx-font-weight: bold;");
+        TextField pollTitleField = new TextField();
         pollTitleField.setPromptText("Poll Title");
-        pollForm.add(new Label("Poll Title:"), 0, 1);
-        pollForm.add(pollTitleField, 1, 1);
+        pollTitleField.setStyle("-fx-background-radius: 20px; -fx-border-radius: 20px;" +
+                "-fx-border-color: #C8F0FF; -fx-padding: 8px; -fx-border-width: 3px;");
+        createPollForm.add(pollTitleLabel, 0, 1);
+        createPollForm.add(pollTitleField, 1, 1);
 
-        pollDescriptionField = new TextField();
-        pollDescriptionField.setPromptText("Poll Description");
-        pollForm.add(new Label("Poll Description:"), 0, 2);
-        pollForm.add(pollDescriptionField, 1, 2);
 
-        startDatePicker = new DatePicker();
-        pollForm.add(new Label("Start Date:"), 0, 3);
-        pollForm.add(startDatePicker, 1, 3);
+        Label pollDescriptionLabel = new Label("Poll Description:");
+        pollDescriptionLabel.setStyle("-fx-font-size: 20px; -fx-font-weight: bold;");
+        TextField pollDescriptionField = new TextField();
+        pollDescriptionField.setPromptText("Enter Poll Description");
+        pollDescriptionField.setStyle("-fx-background-radius: 20px; -fx-border-radius: 20px;" +
+                "-fx-border-color: #C8F0FF; -fx-padding: 8px; -fx-border-width: 3px;");
+        createPollForm.add(pollDescriptionLabel, 0, 2);
+        createPollForm.add(pollDescriptionField, 1, 2);
 
-        endDatePicker = new DatePicker();
-        pollForm.add(new Label("End Date:"), 0, 4);
-        pollForm.add(endDatePicker, 1, 4);
 
-        Button submitButton = new Button("Create Poll");
-        submitButton.setOnAction(event -> createPoll());
-        pollForm.add(submitButton, 0, 5, 2, 1);
+        Label pollStartDateLabel = new Label("Poll Start Date:");
+        pollStartDateLabel.setStyle("-fx-font-size: 20px; -fx-font-weight: bold");
+        DatePicker startDatePicker = new DatePicker();
+        startDatePicker.setStyle("-fx-background-radius: 20px; -fx-border-radius: 20px;" +
+                "-fx-border-color: #C8F0FF; -fx-padding: 8px; -fx-border-width: 3px; -fx-background-color: white");
+        createPollForm.add(pollStartDateLabel, 0, 3);
+        createPollForm.add(startDatePicker, 1, 3);
 
-        layout.setCenter(pollForm);
-    }
 
-    private void createPoll() {
-        JSONObject pollData = new JSONObject();
-        pollData.put("title", pollTitleField.getText());
-        pollData.put("description", pollDescriptionField.getText());
-        pollData.put("start_date", startDatePicker.getValue().toString());
-        pollData.put("end_date", endDatePicker.getValue().toString());
+        Label pollEndDateLabel = new Label("Poll End Date:");
+        pollEndDateLabel.setStyle("-fx-font-size: 20px; -fx-font-weight: bold");
+        DatePicker endDatePicker = new DatePicker();
+        endDatePicker.setStyle("-fx-background-radius: 20px; -fx-border-radius: 20px;" +
+                "-fx-border-color: #C8F0FF; -fx-padding: 8px; -fx-border-width: 3px; -fx-background-color: white");
+        createPollForm.add(pollEndDateLabel, 0, 4);
+        createPollForm.add(endDatePicker, 1, 4);
 
-        boolean success = controller.createPoll(pollData);
-        if (success) {
-            Alert alert = new Alert(Alert.AlertType.INFORMATION);
-            alert.setHeaderText("Success");
-            alert.setContentText("Poll created successfully!");
-            alert.showAndWait();
-        } else {
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setHeaderText("Error");
-            alert.setContentText("Failed to create poll.");
-            alert.showAndWait();
-        }
+
+        // Submit button
+        Button submitButton = new Button("Choose Candidates");
+        submitButton.setStyle("-fx-background-color: #C8F0FF; -fx-text-fill: black; -fx-font-weight: bold;" +
+                "-fx-border-radius: 10px; -fx-font-size: 15px; -fx-cursor: hand");
+        submitButton.setPrefWidth(160);
+        submitButton.setPrefHeight(38);
+        submitButton.setOnAction(event -> {
+            LocalDate startDate = startDatePicker.getValue();
+            LocalDate endDate = endDatePicker.getValue();
+            LocalDate today = LocalDate.now();
+
+            if (pollTitleField.getText().isBlank() || pollDescriptionField.getText().isBlank() || startDate == null || endDate == null) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setHeaderText("Incomplete Form");
+                alert.setContentText("Please ensure all fields are filled.");
+                alert.showAndWait();
+                return;
+            }
+
+            if (endDate.isBefore(startDate) || endDate.isBefore(today)) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setHeaderText("Invalid Dates");
+                alert.setContentText("Ensure dates are valid and not in the past.");
+                alert.showAndWait();
+                return;
+            }
+
+
+            PollModel newPoll = new PollModel(pollTitleField.getText(), pollDescriptionField.getText(),
+                    Date.from(startDate.atStartOfDay(ZoneId.systemDefault()).toInstant()),
+                    Date.from(endDate.atStartOfDay(ZoneId.systemDefault()).toInstant()), user.getUser_ID());
+
+            try {
+                newPoll.setPoll_ID(new PollService().addPoll(newPoll));
+                new CandidateController().displayCandidates(primaryStage, newPoll.getPoll_ID());
+            } catch (Exception e) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setHeaderText("Title Unavailable");
+                alert.setContentText("Choose a different title.");
+                alert.showAndWait();
+            }
+        });
+        createPollForm.add(submitButton, 0, 5);
+        GridPane.setColumnSpan(submitButton, 2);
+        GridPane.setHalignment(submitButton, HPos.CENTER);
+
+
+        // Add form to the layout and set up the scene
+        layout.setCenter(createPollForm);
+        Scene scene = new Scene(layout, Screen.getPrimary().getBounds().getWidth(), Screen.getPrimary().getBounds().getHeight() - 80);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
